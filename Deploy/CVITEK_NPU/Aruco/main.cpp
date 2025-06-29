50710922525a8cd47b0d3aa9e867974f65df3201
ret7020 <ret7020@gmail.com>
2025-06-29 19:14:15 +0300

    Some fixes in just cv aruco detector

--- Deploy/CVITEK_NPU/Aruco/main.cpp
+++ Deploy/CVITEK_NPU/Aruco/main.cpp
@@ -11,6 +11,16 @@
 #define VIDEO_RECORD_FRAME_WIDTH 200
 #define VIDEO_RECORD_FRAME_HEIGHT 200
 
+
+float markerSize_ = 100; // pixels
+std::vector<cv::Point2f> dstPts = {
+    cv::Point2f(0, 0),
+    cv::Point2f(markerSize_ - 1, 0),
+    cv::Point2f(markerSize_ - 1, markerSize_ - 1),
+    cv::Point2f(0, markerSize_ - 1)
+};
+cv::aruco::Dictionary dictionary;
+
 volatile uint8_t interrupted = 0;
 void interrupt_handler(int signum) {
     printf("Signal: %d\n", signum);
@@ -41,22 +51,91 @@ cv::Mat extractMarkerBits(const cv::Mat& markerImage) {
 }
 
 
+
+int detect_marker(cv::Mat *bgr){
+    cv::Mat grayImage, markerImage, thresh;
+    cvtColor(*bgr, grayImage, COLOR_BGR2GRAY);
+    cv::adaptiveThreshold(grayImage, thresh, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 11, 7);
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    cv::findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+    for (size_t i = 0; i < contours.size(); ++i) {
+        if (hierarchy[i][3] != -1)
+            continue;
+
+        double area = cv::contourArea(contours[i]);
+        if (area < 100)
+            continue; // Skip small noise
+
+        vector<Point> approx;
+        cv::approxPolyDP(contours[i], approx, cv::arcLength(contours[i], true) * 0.02, true);
+
+        // Search quadrilaterals
+        if (approx.size() == 4 && cv::isContourConvex(approx)) {
+            Rect bound = cv::boundingRect(approx);
+            float aspectRatio = (float)bound.width / bound.height;
+            if (aspectRatio < 0.8 || aspectRatio > 1.2)
+                continue;
+
+            // // Draw corners
+            // for (int i = 0; i < 4; i++) {
+            //     cv::circle(bgr, approx[i], 5, Scalar(128), FILLED);
+            // }
+
+            // printf("Corners:\n");
+            // for (const Point &pt : approx) {
+            //     printf("x=%d; y=%d\n", pt.x, pt.y);
+            // }
+
+            std::vector<cv::Point2f> srcPts;
+            for (const cv::Point &pt : approx)
+                srcPts.push_back(pt);
+            
+            cv::Point2f center(0, 0);
+            for (const cv::Point2f &pt : srcPts)
+                center += pt;
+            center *= (1.0 / srcPts.size());
+
+            // Sort: top-left, top-right, bottom-right, bottom-left
+            std::vector<cv::Point2f> orderedPts(4);
+            for (const cv::Point2f &pt : srcPts) {
+                if (pt.x < center.x && pt.y < center.y)
+                    orderedPts[0] = pt; // Top-left
+                else if (pt.x > center.x && pt.y < center.y)
+                    orderedPts[1] = pt; // Top-right
+                else if (pt.x > center.x && pt.y > center.y)
+                    orderedPts[2] = pt; // Bottom-right
+                else if (pt.x < center.x && pt.y > center.y)
+                    orderedPts[3] = pt; // Bottom-left
+            }
+
+            cv::Mat warpMat = cv::getPerspectiveTransform(orderedPts, dstPts);
+            cv::warpPerspective(grayImage, markerImage, warpMat, cv::Size(markerSize_, markerSize_));
+            threshold(markerImage, thresh, 127, 255, THRESH_BINARY);
+
+            // resize(thresh, thresh, Size(70, 70), 0, 0, cv::INTER_NEAREST);
+            cv::Mat bitsMat = extractMarkerBits(thresh);
+            
+            int id = -1, rotation = -1;
+            bool valid = dictionary.identify(bitsMat, id, rotation, false);
+
+            return id;
+
+        }
+    }
+
+    return -1;
+}
+
+
 int main() {
     signal(SIGINT, interrupt_handler);
     // Camera init
     cv::VideoCapture cap;
     MJPEGWriter test(7777);
 
-    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_50);
-    cv::Mat grayImage, markerImage, thresh;
-
-    float markerSize_ = 100; // pixels
-    std::vector<cv::Point2f> dstPts = {
-        cv::Point2f(0, 0),
-        cv::Point2f(markerSize_ - 1, 0),
-        cv::Point2f(markerSize_ - 1, markerSize_ - 1),
-        cv::Point2f(0, markerSize_ - 1)
-    };
+    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_50);
 
     cap.set(cv::CAP_PROP_FRAME_WIDTH, VIDEO_RECORD_FRAME_WIDTH);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, VIDEO_RECORD_FRAME_HEIGHT);
@@ -83,76 +162,14 @@ int main() {
             continue;
         }
 
-        cvtColor(bgr, grayImage, COLOR_BGR2GRAY);
-        cv::adaptiveThreshold(grayImage, thresh, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 11, 7);
-        vector<vector<Point>> contours;
-        vector<Vec4i> hierarchy;
-        cv::findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+        auto begin = std::chrono::steady_clock::now();
+        int id = detect_marker(&bgr);
+        auto end = std::chrono::steady_clock::now();
+        double fps = 1 / std::chrono::duration<double>(end - begin).count();
+        printf("Detector FPS: %lf\n", fps);
+        printf("Detected ID: %d\n", id);
 
-        for (size_t i = 0; i < contours.size(); ++i) {
-            if (hierarchy[i][3] != -1)
-                continue;
-
-            double area = cv::contourArea(contours[i]);
-            if (area < 100)
-                continue; // Skip small noise
-
-            vector<Point> approx;
-            cv::approxPolyDP(contours[i], approx, cv::arcLength(contours[i], true) * 0.02, true);
-
-            // Search quadrilaterals
-            if (approx.size() == 4 && cv::isContourConvex(approx)) {
-                Rect bound = cv::boundingRect(approx);
-                float aspectRatio = (float)bound.width / bound.height;
-                if (aspectRatio < 0.8 || aspectRatio > 1.2)
-                    continue;
-
-                // // Draw corners
-                for (int i = 0; i < 4; i++) {
-                    cv::circle(bgr, approx[i], 5, Scalar(128), FILLED);
-                }
-
-                // printf("Corners:\n");
-                // for (const Point &pt : approx) {
-                //     printf("x=%d; y=%d\n", pt.x, pt.y);
-                // }
-
-                std::vector<cv::Point2f> srcPts;
-                for (const cv::Point &pt : approx)
-                    srcPts.push_back(pt);
-                
-                cv::Point2f center(0, 0);
-                for (const cv::Point2f &pt : srcPts)
-                    center += pt;
-                center *= (1.0 / srcPts.size());
-
-                // Sort: top-left, top-right, bottom-right, bottom-left
-                std::vector<cv::Point2f> orderedPts(4);
-                for (const cv::Point2f &pt : srcPts) {
-                    if (pt.x < center.x && pt.y < center.y)
-                        orderedPts[0] = pt; // Top-left
-                    else if (pt.x > center.x && pt.y < center.y)
-                        orderedPts[1] = pt; // Top-right
-                    else if (pt.x > center.x && pt.y > center.y)
-                        orderedPts[2] = pt; // Bottom-right
-                    else if (pt.x < center.x && pt.y > center.y)
-                        orderedPts[3] = pt; // Bottom-left
-                }
-
-                cv::Mat warpMat = cv::getPerspectiveTransform(orderedPts, dstPts);
-                cv::warpPerspective(grayImage, markerImage, warpMat, cv::Size(markerSize_, markerSize_));
-                threshold(markerImage, thresh, 127, 255, THRESH_BINARY);
-
-                // resize(thresh, thresh, Size(70, 70), 0, 0, cv::INTER_NEAREST);
-                cv::Mat bitsMat = extractMarkerBits(thresh);
-                
-                int id = -1, rotation = -1;
-                bool valid = dictionary.identify(bitsMat, id, rotation, false);
-
-                printf("ID: %d\n", id);
-
-            }
-        }
+        
 
         test.write(bgr);
         bgr.release();
